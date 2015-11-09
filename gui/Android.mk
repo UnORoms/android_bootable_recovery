5c98cd44f70115cd9be7309ef1e7fda2d9ae58ab
Shantur Rathore <shantur.rathore@gmail.com>
2015-11-09 15:17:40 +0000

    Remove dependencies on external/stlport that causes issues to build with CM-13.0

--- gui/Android.mk
+++ gui/Android.mk
@@ -69,7 +69,7 @@ ifeq ($(TW_ROUND_SCREEN), true)
     LOCAL_CFLAGS += -DTW_ROUND_SCREEN
 endif
 
-LOCAL_C_INCLUDES += bionic external/stlport/stlport system/core/libpixelflinger/include
+LOCAL_C_INCLUDES += bionic system/core/libpixelflinger/include
 LOCAL_CFLAGS += -DTWRES=\"$(TWRES_PATH)\"
 
 include $(BUILD_STATIC_LIBRARY)
